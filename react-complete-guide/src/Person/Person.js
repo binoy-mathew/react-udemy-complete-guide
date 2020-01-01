b27b9a2e3eaacf21518920bd707d013a25686cd9
bmatt <binoymathew9@gmail.com>
2020-01-02 00:35:07 +0530

    Simple dynamism in Person component

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>This is a Person</p>
+    return <p>This is a Person and I am {Math.floor(Math.random() * 20)} years old</p>
 }
 
 export default person;
