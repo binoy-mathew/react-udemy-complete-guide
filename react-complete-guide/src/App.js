39000f08593814865e164ccca8a012a2b83c0281
bmatt <binoymathew9@gmail.com>
2020-01-01 19:09:18 +0530

    Using React.createElement instead of writing html.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi!! I am a React App. </h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi!! I am a React App. </h1>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement('hi', null, 'this is inside createElement'));
   }
 }
 
