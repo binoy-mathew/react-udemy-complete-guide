ef9154791a034ac87665d4f42aa8e2c8b5726e69
binoy-mathew <binoymathew9@gmail.com>
2020-12-04 00:27:03 +0530

    React Hook useEffect added in function component

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const cockpit = (props) => {
+    useEffect(() => {
+            console.log('[Cockpit.js] useEffect');
+            return () => {
+                console.log('[Cockpit.js] Clean up work');
+            }
+    }, []);
+
     return (
         <div>
             <h1>{props.appTitle}</h1>
