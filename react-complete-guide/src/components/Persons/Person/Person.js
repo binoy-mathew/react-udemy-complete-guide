966a2eb78f2385050c4a4f57546a207148bb9374
binoy-mathew <binoymathew9@gmail.com>
2020-12-06 01:03:58 +0530

    Add a Higher Order Component for containing JSX code

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/aux';
 
 import './Person.css';
 
@@ -6,11 +7,12 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className="Person">
+            // <div className="Person">
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
         )
     }
 
