1b696db5db2e9790273e3cefba03564a8558ef5f
binoy-mathew <binoymathew9@gmail.com>
2020-12-29 20:07:34 +0530

    Added simple Route to a component

--- section-9-http-calls/src/containers/Blog/Blog.js
+++ section-9-http-calls/src/containers/Blog/Blog.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 
@@ -19,7 +22,14 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {
+                /*
+                <Route path="/" exact render={() => <h1>Binoy</h1>} />
+                <Route path="/" render={() => <h1>Mathew</h1>} />
+                */
+                }
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
