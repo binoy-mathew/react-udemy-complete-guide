aed4a303bb65a23d74bd01f1b9d162ed7facabec
binoy-mathew <binoymathew9@gmail.com>
2021-01-01 07:24:24 +0530

    Dynamic Route Parameters and Switch Component

--- section-9-http-calls/src/containers/Blog/Blog.js
+++ section-9-http-calls/src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 
@@ -32,9 +33,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
