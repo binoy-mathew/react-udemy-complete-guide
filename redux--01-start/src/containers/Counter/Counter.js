99d15bafa8563cf3cfc4d3cf9b8bebc7682f3f19
Binoy Mathew <binoymathew9@gmail.com>
2021-02-09 00:26:45 +0530

    Store Result and delete from state implemented, action types defn outsourced

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import * as actionTypes from '../../store/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -18,6 +19,12 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>)}
+                </ul>
             </div>
         );
     }
@@ -33,7 +40,8 @@ which will be made available to the current component as a "ctr" props.
 */
 const mapStateToProps = state => {
     return {
-        ctr : state.counter
+        ctr : state.counter,
+        storedResults : state.results
     };
 };
 
@@ -45,10 +53,12 @@ and optionally any number of additional parameters that you need.
 */
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter : () => dispatch({ type : 'INCREMENT' }),
-        onDecrementCounter : () => dispatch({ type : 'DECREMENT' }),
-        onAddCounter : () => dispatch({ type : 'ADD', value : 5 }),
-        onSubtractCounter : () => dispatch({ type : 'SUBTRACT' , value : 5})
+        onIncrementCounter : () => dispatch({ type : actionTypes.INCREMENT }),
+        onDecrementCounter : () => dispatch({ type : actionTypes.DECREMENT }),
+        onAddCounter : () => dispatch({ type : actionTypes.ADD, value : 5 }),
+        onSubtractCounter : () => dispatch({ type : actionTypes.SUBTRACT , value : 5}),
+        onStoreResult : () => dispatch({ type : actionTypes.STORE_RESULT}),
+        onDeleteResult : (index) => dispatch({ type : actionTypes.DELETE_RESULT, resultElementId : index})
     };
 };
 
--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,28 +1,60 @@
+import * as actionTypes from './actions';
 
 const initialState = {
-    counter : 0
+    counter : 0,
+    results : []
 };
 
 const reducer = (state = initialState, action) => {
     switch( action.type ) {
-        case 'INCREMENT':
-            return {
-                counter : state.counter + 1
-            };
+        case actionTypes.INCREMENT:
+            /*
+            Ensure that you change state immutably. One way of doing that is to use Object.assign()
+            Note however that though Object.assign will return a new object, the array within that new object is still pointing to the old array. 
+            i.e., the new object is a shallow copy of the original object.
+            But in this case, it is ok, because we are modifying the counter value.
+            */
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
         
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
+            //A shorter way of creating a new Object is to use the spread operator. Followed by the property that you want to add or overwrite.
+            //Again, it will be a shallow copy.
             return {
+                ...state,
                 counter : state.counter - 1
             };
         
-        case 'ADD':
+        case actionTypes.ADD:
             return {
+                ...state,
                 counter : state.counter + action.value
             };
 
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
+                ...state,
                 counter : state.counter - action.value
+            };
+
+        case actionTypes.STORE_RESULT:
+            /*
+            In this case, it is not enough to create a shallow copy. We need to copy the array into a separate array.
+            The way we do it is by using concat() which returns a new array (as opposed to push() which updates the original array).
+            */
+            return {
+                ...state,
+                results : state.results.concat({id : new Date(), value : state.counter})
+            };
+
+        case actionTypes.DELETE_RESULT:
+            //Dont do a state.results.splice on the existing array because that modifies the existing array. Instead create a new array and splice
+            //Or you can use array.filter 
+            const newArray = state.results.filter(e => e.id !== action.resultElementId);
+            return {
+                ...state,
+                results : newArray
             };
     }
     
