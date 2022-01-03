498d5196337b4a0754b12c6096b161c67bad597d
Binoy Mathew <binoymathew9@gmail.com>
2022-01-03 09:47:29 +0530

    Passing in props and using a variable to create the props

--- v2-react-complete-guide/starter-project/src/components/ExpenseItem.js
+++ v2-react-complete-guide/starter-project/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    console.log(props.title);
+
     return (
         <div className="expense-item">
-            <div>Oct 31 2021</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$Painful Loss</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
