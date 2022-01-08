709421ec1f07dbb740fc29b6508dd1323dd44a80
Binoy Mathew <binoymathew9@gmail.com>
2022-01-08 18:22:58 +0530

    Usage of useState() - basic scenario

--- v2-react-complete-guide/react-state/src/components/Expenses/ExpenseItem.js
+++ v2-react-complete-guide/react-state/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-    </Card>
-  );
-}
+
+	const [title, setTitle] = useState(props.title);
+
+	const clickHandler = (e) => {
+		setTitle('Updated');
+		console.log(title); // This will display the old value because update to state is NOT synchronous.
+	}
+
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
+    );
+};
 
 export default ExpenseItem;
