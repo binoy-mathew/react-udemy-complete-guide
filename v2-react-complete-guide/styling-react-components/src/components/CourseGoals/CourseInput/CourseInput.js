1ecee45e42bc36d8ee47eec776cee4c6cca117b3
Binoy Mathew <binoymathew9@gmail.com>
2022-01-13 23:42:56 +0530

    Styled Components - Basic implementation

--- v2-react-complete-guide/styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ v2-react-complete-guide/styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,18 +26,11 @@ const CourseInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label style={{ color: !isValid ? 'red' : 'black' }}>
+            <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+                <label className={`form-control ${!isValid ? 'invalid' : ''}`}>
                     Course Goal
                 </label>
-                <input
-                    style={{
-                        borderColor: !isValid ? 'red' : 'black',
-                        background: !isValid ? 'salmon' : 'transparent',
-                    }}
-                    type="text"
-                    onChange={goalInputChangeHandler}
-                />
+                <input  type="text" onChange={goalInputChangeHandler} />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
