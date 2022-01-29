c7dffebd5ae1b8c07f892f1aa3deb7aaa190cd4f
Binoy Mathew <binoymathew9@gmail.com>
2022-01-29 22:49:41 +0530

    componentWillUnmount() added

--- v2-react-complete-guide/class-based-componenents/src/components/User.js
+++ v2-react-complete-guide/class-based-componenents/src/components/User.js
@@ -2,6 +2,10 @@ import React from 'react';
 import classes from './User.module.css';
 
 class User extends React.Component {
+	componentWillUnmount() {
+		console.log('Unmounting component...');
+	}
+
 	render() {
 		return <li className={classes.user}>{this.props.name}</li>;
 	}
