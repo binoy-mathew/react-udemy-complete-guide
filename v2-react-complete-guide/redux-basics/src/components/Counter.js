9ded2e35ddc8e97a940d9844ab567f95718bf5be
Binoy Mathew <binoymathew9@gmail.com>
2022-02-09 10:31:55 +0530

    Class Based components with Redux

--- v2-react-complete-guide/redux-basics/src/components/Counter.js
+++ v2-react-complete-guide/redux-basics/src/components/Counter.js
@@ -1,6 +1,8 @@
 import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { Component } from 'react';
 
+/*
 const Counter = () => {
   const toggleCounterHandler = () => {};
   const dispatch = useDispatch();
@@ -32,5 +34,63 @@ const Counter = () => {
     </main>
   );
 };
+*/
 
-export default Counter;
+
+class Counter extends Component {
+  incrementHandler(by) {
+    console.log('increment by ', by);
+    this.props.increment(by);
+  }
+
+  decrementHandler(by) {
+    console.log('decrement by ', by);
+    this.props.decrement(by);
+  }
+
+  toggleCounterHandler() {
+
+  }
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this, 3)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this, 2)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+
+//First argument to connect is a function which maps redux state to props which will be received by Counter component
+//This function should return an object with the slice of the state that the Counter component needs.
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  };
+};
+
+//Second argument to connect is a function,
+//Return an object where the key is a prop name that we can use within the Counter component.
+//Value is a function in which we call the dispatch function with the required action.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: (by) => dispatch({ type: 'increment', by }),
+    decrement: (by) => dispatch({ type: 'decrement', by })
+  };
+};
+
+
+//The way we use connect is to execute the function that connect returns.
+//connect is a higher order component.
+//connect takes 2 arguments, both functions.
+//this will also set up a subscription to the store.
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+
--- v2-react-complete-guide/redux-basics/src/store/index.js
+++ v2-react-complete-guide/redux-basics/src/store/index.js
@@ -1,14 +1,15 @@
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0}, action) => {
+    console.log('in store by value is ', action.by);
     if (action.type === 'increment') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + (action.by ? action.by : 1)
         };
     }
     else if (action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - (action.by ? action.by : 1)
         };
     }
   
