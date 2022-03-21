9b50079c749bb726e25fba8eeecba7a4466a783f
Binoy Mathew <binoymathew9@gmail.com>
2022-03-21 10:35:35 +0530

    Mocking API calls using fetch()

--- v2-react-complete-guide/testing-react-apps/src/components/Async.test.js
+++ v2-react-complete-guide/testing-react-apps/src/components/Async.test.js
@@ -4,6 +4,10 @@ import Async from "./Async";
 describe('Async Component', () => {
     test('renders post if request succeeds', async () => {
         //Arrange
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async() => [{ id: 'p1', title: 'First post'}]
+        });
         render(<Async />);
 
         //Act
