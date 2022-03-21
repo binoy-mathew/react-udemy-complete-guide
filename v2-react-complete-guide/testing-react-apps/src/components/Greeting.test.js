d57db931b41fdaeab97283f69d9fdfdcc7f5e8f8
Binoy Mathew <binoymathew9@gmail.com>
2022-03-21 09:04:51 +0530

    Set up a test suite using describe()

--- v2-react-complete-guide/testing-react-apps/src/components/Greeting.test.js
+++ v2-react-complete-guide/testing-react-apps/src/components/Greeting.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test('renders Hello World', () => {
-    //Arrange
-    render(<Greeting />);
-
-    //Act
-    // ... nothing
-
-    //Assert
-    const helloWorldElement = screen.getByText('Hello World', { exact: false});
-    expect(helloWorldElement).toBeInTheDocument();
+describe('Greeting Component', () => {
+    test('renders Hello World', () => {
+        //Arrange
+        render(<Greeting />);
+    
+        //Act
+        // ... nothing
+    
+        //Assert
+        const helloWorldElement = screen.getByText('Hello World', { exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    });
 });
+
+
